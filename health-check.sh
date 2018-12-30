f18a9946e34777f13b9b6f0d2115114a4857be77
Sadashiva Murthy M <mssmurthy.tech@gmail.com>
2018-12-30 23:36:28 +0530

    Update health-check.sh

--- health-check.sh
+++ health-check.sh
@@ -1,106 +1,91 @@
 #!/bin/bash
 ##---------- Author : Sadashiva Murthy M ----------------------------------------------------##
-##---------- Blog site : http://simplylinuxfaq.blogspot.in ----------------------------------##
+##---------- Blog site : https://www.simplylinuxfaq.com -------------------------------------##
 ##---------- Github page : https://github.com/SimplyLinuxFAQ/health-check-script ------------##
 ##---------- Purpose : To quickly check and report health status in a linux system.----------##
-##---------- Tested on : RHEL7/6/5/, SLES12/11, Ubuntu14, Mint16, Boss6(Debian) variants.----##
-##---------- Updated version : v1.0 (Updated on 25th-June-2017) -----------------------------##
-##-----NOTE: This script requires root privileges, otherwise you could run the script -------##
+##---------- Tested on : RHEL8(beta)/7/6/5/, SLES/SLED 12/11, Ubuntu14/16/18, Mint16, -------## 
+##---------- Boss6(Debian) variants. It may work on other vari as well, but not tested. -----##
+##---------- Updated version : v2.0 (Updated on 30th Dec 2018) ------------------------------##
+##-----NOTE: This script requires root privileges, otherwise one could run the script -------##
 ##---- as a sudo user who got root privileges. ----------------------------------------------##
 ##----------- "sudo /bin/bash <ScriptName>" -------------------------------------------------##
 
+#------variables used------#
 S="************************************"
 D="-------------------------------------"
+COLOR="y"
 
-MOUNT=$(mount|egrep -iw "ext4|ext3|xfs|gfs|gfs2|btrfs"|sort -u -t' ' -k1,2)
-FS_USAGE=$(df -PTh|egrep -iw "ext4|ext3|xfs|gfs|gfs2|btrfs"|sort -k6n|awk '!seen[$1]++')
-IUSAGE=$(df -PThi|egrep -iw "ext4|ext3|xfs|gfs|gfs2|btrfs"|sort -k6n|awk '!seen[$1]++')
+MOUNT=$(mount|egrep -iw "ext4|ext3|xfs|gfs|gfs2|btrfs"|grep -v "loop"|sort -u -t' ' -k1,2)
+FS_USAGE=$(df -PTh|egrep -iw "ext4|ext3|xfs|gfs|gfs2|btrfs"|grep -v "loop"|sort -k6n|awk '!seen[$1]++')
+IUSAGE=$(df -PThi|egrep -iw "ext4|ext3|xfs|gfs|gfs2|btrfs"|grep -v "loop"|sort -k6n|awk '!seen[$1]++')
 
-#--------Checking the availability of sysstat package..........#
-if [ ! -x /usr/bin/mpstat ]
-then
-    printf "\nError : Either \"mpstat\" command not available OR \"sysstat\" package is not properly installed. Please make sure this package is installed and working properly!, then run this script.\n\n"
-    exit 1
-fi
-
-echo -e "$S Health Status Report $S"
-echo -e "\nOperating System Details" 
-echo -e "$D"
-printf "Hostname :" $(hostname -f > /dev/null 2>&1) && printf " $(hostname -f)" || printf " $(hostname -s)"
-
-[ -x /usr/bin/lsb_release ] &&  echo -e "\nOperating System :" $(lsb_release -d|awk -F: '{print $2}'|sed -e 's/^[ \t]*//')  || echo -e "\nOperating System :" $(cat /etc/system-release)
-echo -e "Kernel Version :" $(uname -r) 
-printf "OS Architecture :" $(arch | grep x86_64 2>&1 > /dev/null) && printf " 64 Bit OS\n"  || printf " 32 Bit OS\n"
-
-#--------Print system uptime-------#
-UPTIME=$(uptime)
-echo $UPTIME|grep day 2>&1 > /dev/null
-if [ $? != 0 ]
-then
-  echo $UPTIME|grep -w min 2>&1 > /dev/null && echo -e "System Uptime : "$(echo $UPTIME|awk '{print $2" by "$3}'|sed -e 's/,.*//g')" minutes"  || echo -e "System Uptime : "$(echo $UPTIME|awk '{print $2" by "$3" "$4}'|sed -e 's/,.*//g')" hours" 
+if [ $COLOR == y ]; then
+{
+ GCOLOR="\e[47;32m ------ OK/HEALTHY \e[0m"
+ WCOLOR="\e[43;31m ------ WARNING \e[0m"
+ CCOLOR="\e[47;31m ------ CRITICAL \e[0m"
+}
 else
-  echo -e "System Uptime :" $(echo $UPTIME|awk '{print $2" by "$3" "$4" "$5" hours"}'|sed -e 's/,//g') 
+{
+ GCOLOR=" ------ OK/HEALTHY "
+ WCOLOR=" ------ WARNING "
+ CCOLOR=" ------ CRITICAL "
+}
 fi
-echo -e "Current System Date & Time : "$(date +%c)
 
+echo -e "$S"
+echo -e "\tSystem Health Status"
+echo -e "$S"
 
 #--------Check for any read-only file systems--------#
 echo -e "\nChecking For Read-only File System[s]"
 echo -e "$D"
 echo "$MOUNT"|grep -w \(ro\) && echo -e "\n.....Read Only file system[s] found"|| echo -e ".....No read-only file system[s] found. "
 
-
 #--------Check for currently mounted file systems--------#
 echo -e "\n\nChecking For Currently Mounted File System[s]"
 echo -e "$D$D"
 echo "$MOUNT"|column -t
-
 
 #--------Check disk usage on all mounted file systems--------#
 echo -e "\n\nChecking For Disk Usage On Mounted File System[s]"
 echo -e "$D$D"
 echo -e "( 0-90% = OK/HEALTHY, 90-95% = WARNING, 95-100% = CRITICAL )"
 echo -e "$D$D"
-echo -e "Mounted File System[s] Utilization (Percentage Used):\n" 
+echo -e "Mounted File System[s] Utilization (Percentage Used):\n"
 
-echo "$FS_USAGE"|awk '{print $1 " "$7}' > /tmp/s1.out
-echo "$FS_USAGE"|awk '{print $6}'|sed -e 's/%//g' > /tmp/s2.out
-> /tmp/s3.out
+COL1=$(echo "$FS_USAGE"|awk '{print $1 " "$7}')
+COL2=$(echo "$FS_USAGE"|awk '{print $6}'|sed -e 's/%//g')
 
-for i in $(cat /tmp/s2.out);
-do
+for i in $(echo "$COL2"); do
 {
-  if [ $i -ge 95 ];
-   then
-     echo -e $i"% ------------------Critical" >> /tmp/s3.out;
-   elif [[ $i -ge 90 && $i -lt 95 ]];
-   then
-     echo -e $i"% ------------------Warning" >> /tmp/s3.out; 
-   else
-     echo -e $i"% ------------------Good/Healthy" >> /tmp/s3.out;
+  if [ $i -ge 95 ]; then
+    COL3="$(echo -e $i"% $CCOLOR\n$COL3")"
+  elif [[ $i -ge 90 && $i -lt 95 ]]; then
+    COL3="$(echo -e $i"% $WCOLOR\n$COL3")"
+  else
+    COL3="$(echo -e $i"% $GCOLOR\n$COL3")"
   fi
-} 
+}
 done
-paste -d"\t" /tmp/s1.out /tmp/s3.out|column -t
+COL3=$(echo "$COL3"|sort -k1n)
+paste  <(echo "$COL1") <(echo "$COL3") -d' '|column -t
 
 #--------Check for any zombie processes--------#
 echo -e "\n\nChecking For Zombie Processes"
 echo -e "$D"
-ps -eo stat|grep -w Z 1>&2 > /dev/null 
-if [ $? == 0 ]
-then
-  echo -e "Number of zombie process on the system are :" $(ps -eo stat|grep -w Z|wc -l) 
-  echo -e "\n  Details of each zombie processes found	"
+ps -eo stat|grep -w Z 1>&2 > /dev/null
+if [ $? == 0 ]; then
+  echo -e "Number of zombie process on the system are :" $(ps -eo stat|grep -w Z|wc -l)
+  echo -e "\n  Details of each zombie processes found   "
   echo -e "  $D"
   ZPROC=$(ps -eo stat,pid|grep -w Z|awk '{print $2}')
-  for i in $(echo "$ZPROC")
-  do
+  for i in $(echo "$ZPROC"); do
       ps -o pid,ppid,user,stat,args -p $i
   done
 else
  echo -e "No zombie processes found on the system."
 fi
-
 #--------Check Inode usage--------#
 echo -e "\n\nChecking For INode Usage"
 echo -e "$D$D"
@@ -108,63 +93,41 @@ echo -e "( 0-90% = OK/HEALTHY, 90-95% = WARNING, 95-100% = CRITICAL )"
 echo -e "$D$D"
 echo -e "INode Utilization (Percentage Used):\n"
 
-echo "$IUSAGE"|awk '{print $1" "$7}' > /tmp/s1.out
-echo "$IUSAGE"|awk '{print $6}'|sed -e 's/%//g' > /tmp/s2.out
-> /tmp/s3.out
+COL11=$(echo "$IUSAGE"|awk '{print $1" "$7}')
+COL22=$(echo "$IUSAGE"|awk '{print $6}'|sed -e 's/%//g')
 
-for i in $(cat /tmp/s2.out);
-do
-  if [[ $i = *[[:digit:]]* ]];
-  then
+for i in $(echo "$COL22"); do
+{
+  if [[ $i = *[[:digit:]]* ]]; then
   {
-  if [ $i -ge 95 ];
-  then
-    echo -e $i"% ------------------Critical" >> /tmp/s3.out;
-  elif [[ $i -ge 90 && $i -lt 95 ]];
-  then
-    echo -e $i"% ------------------Warning" >> /tmp/s3.out;
+  if [ $i -ge 95 ]; then
+    COL33="$(echo -e $i"% $CCOLOR\n$COL33")"
+  elif [[ $i -ge 90 && $i -lt 95 ]]; then
+    COL33="$(echo -e $i"% $WCOLOR\n$COL33")"
   else
-    echo -e $i"% ------------------Good/Healthy" >> /tmp/s3.out;
+    COL33="$(echo -e $i"% $GCOLOR\n$COL33")"
   fi
   }
   else
-    echo -e $i"% (Inode Percentage details not available)" >> /tmp/s3.out
+    COL33="$(echo -e $i"% (Inode Percentage details not available)\n$COL33")"
   fi
+}
 done
-paste -d"\t" /tmp/s1.out /tmp/s3.out|column -t
 
-
-#--------Check for RAM Utilization--------#
-MEM_DETAILS=$(cat /proc/meminfo)
-echo -e "\n\nChecking Memory Usage Details"
-echo -e "$D"
-echo -e "Total RAM (/proc/meminfo) : "$(echo "$MEM_DETAILS"|grep MemTotal|awk '{print $2/1024}') "MB OR" $(echo "$MEM_DETAILS"|grep MemTotal|awk '{print $2/1024/1024}') "GB"
-echo -e "Used RAM in MB : "$(free -m|grep -w Mem:|awk '{print $3}')", in GB : "$(free -m|grep -w Mem:|awk '{print $3/1024}')
-echo -e "Free RAM in MB : "$(echo "$MEM_DETAILS"|grep -w MemFree|awk '{print $2/1024}')" , in GB : "$(echo "$MEM_DETAILS"|grep -w MemFree |awk '{print $2/1024/1024}')
+COL33=$(echo "$COL33"|sort -k1n)
+paste  <(echo "$COL11") <(echo "$COL33") -d' '|column -t
 
 #--------Check for SWAP Utilization--------#
 echo -e "\n\nChecking SWAP Details"
 echo -e "$D"
-echo -e "Total Swap Memory in MB : "$(echo "$MEM_DETAILS"|grep -w SwapTotal|awk '{print $2/1024}')", in GB : "$(echo "$MEM_DETAILS"|grep -w SwapTotal|awk '{print $2/1024/1024}')
-echo -e "Swap Free Memory in MB : "$(echo "$MEM_DETAILS"|grep -w SwapFree|awk '{print $2/1024}')", in GB : "$(echo "$MEM_DETAILS"|grep -w SwapFree|awk '{print $2/1024/1024}')
+echo -e "Total Swap Memory in MiB : "$(grep -w SwapTotal /proc/meminfo|awk '{print $2/1024}')", in GiB : "\
+$(grep -w SwapTotal /proc/meminfo|awk '{print $2/1024/1024}')
+echo -e "Swap Free Memory in MiB : "$(grep -w SwapFree /proc/meminfo|awk '{print $2/1024}')", in GiB : "\
+$(grep -w SwapFree /proc/meminfo|awk '{print $2/1024/1024}')
 
 #--------Check for Processor Utilization (current data)--------#
 echo -e "\n\nChecking For Processor Utilization"
 echo -e "$D"
-echo -e "Manufacturer: "$(dmidecode -s processor-manufacturer|uniq)
-echo -e "Processor Model: "$(dmidecode -s processor-version|uniq)
-if [ -e /usr/bin/lscpu ]
-then
-{
-	echo -e "No. Of Processor(s) :" $(lscpu|grep -w "Socket(s):"|awk -F: '{print $2}') 
-	echo -e "No. of Core(s) per processor :" $(lscpu|grep -w "Core(s) per socket:"|awk -F: '{print $2}') 
-}
-else
-{
-	echo -e "No. Of Processor(s) Found :" $(grep -c processor /proc/cpuinfo) 
-	echo -e "No. of Core(s) per processor :" $(grep "cpu cores" /proc/cpuinfo|uniq|wc -l) 
-}
-fi
 echo -e "\nCurrent Processor Utilization Summary :\n"
 mpstat|tail -2
 
@@ -175,13 +138,15 @@ echo -e "Current Load Average : $(uptime|grep -o "load average.*"|awk '{print $3
 
 #------Print most recent 3 reboot events if available----#
 echo -e "\n\nMost Recent 3 Reboot Events"
-echo -e "$D$D" 
-last -x 2> /dev/null|grep reboot 1> /dev/null && /usr/bin/last -x 2> /dev/null|grep reboot|head -3 || echo -e "No reboot events are recorded."
+echo -e "$D$D"
+last -x 2> /dev/null|grep reboot 1> /dev/null && /usr/bin/last -x 2> /dev/null|grep reboot|head -3 || \
+echo -e "No reboot events are recorded."
 
 #------Print most recent 3 shutdown events if available-----#
 echo -e "\n\nMost Recent 3 Shutdown Events"
 echo -e "$D$D"
-last -x 2> /dev/null|grep shutdown 1> /dev/null && /usr/bin/last -x 2> /dev/null|grep shutdown|head -3 || echo -e "No shutdown events are recorded."
+last -x 2> /dev/null|grep shutdown 1> /dev/null && /usr/bin/last -x 2> /dev/null|grep shutdown|head -3 || \
+echo -e "No shutdown events are recorded."
 
 #--------Print top 5 most memory consuming resources---------#
 echo -e "\n\nTop 5 Memory Resource Hog Processes"
@@ -192,3 +157,10 @@ ps -eo pmem,pcpu,pid,ppid,user,stat,args | sort -k 1 -r | head -6|sed 's/$/\n/'
 echo -e "\n\nTop 5 CPU Resource Hog Processes"
 echo -e "$D$D"
 ps -eo pcpu,pmem,pid,ppid,user,stat,args | sort -k 1 -r | head -6|sed 's/$/\n/'
+
+echo -e "NOTE:- If any of the above fields are marked as \"blank\" or \"NONE\" or \"UNKNOWN\" or \"Not Available\" or \"Not Specified\"
+that means either there is no value present in the system for these fields, otherwise that value may not be available,
+or suppressed since there was an error in fetching details."
+echo -e "\n\t\t %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
+echo -e "\t\t   <>--------<> Powered By : https://www.simplylinuxfaq.com <>--------<>"
+echo -e "\t\t %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
